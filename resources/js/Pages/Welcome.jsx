14147fda51bfcd60838aca864a6a4ffc70025aaf
palagashviliniika <n_palagashvili@cu.edu.ge>
2023-02-05 00:51:23 +0400

    Started working on project

---
+++ resources/js/Pages/Welcome.jsx
@@ -0,0 +1,11 @@
+import { Link, Head } from '@inertiajs/react';
+
+export default function Welcome(props) {
+    console.log(props)
+    return (
+        <>
+            <Head title="Welcome" />
+
+        </>
+    );
+}
